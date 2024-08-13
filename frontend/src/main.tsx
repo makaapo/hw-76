27d775007b3d667be28120137425598704ccb53e
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-08-13 12:36:55 +0600

    добавил во frontend - hooks и store для redux, примерные типы и axiosApi отобразил все в main, в api добавил файл nodemon.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,16 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App';
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import App from './App'
+import {Provider} from 'react-redux';
+import 'react-toastify/dist/ReactToastify.min.css';
+import {store} from './app/store';
+import {ToastContainer} from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App/>
-  </StrictMode>,
-);
+    <Provider store={store}>
+      <ToastContainer position="bottom-right" />
+      <App />
+    </Provider>
+  </StrictMode>
+)
---
+++ frontend/src/types.ts
@@ -0,0 +1,11 @@
+export interface IMessageForm {
+  author: string;
+  message: string;
+}
+
+export interface IMessage {
+  id: string;
+  author: string;
+  message: string;
+  datetime: string;
+}
